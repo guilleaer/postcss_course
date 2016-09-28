2bae262180c1e7f94329962bbdece51583ddfd7c
guilleaer <guille.aer@gmail.com>
2016-09-28 13:03:13 +0200

    add plugin autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
-    postcss = require('gulp-postcss');
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer');
  
 gulp.task('css', function () {
   var processors = [
+    autoprefixer
   ];
   return gulp.src('./src/css/*.css')
     .pipe(postcss(processors))
