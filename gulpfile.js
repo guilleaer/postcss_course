131d58b2c80d67152013be46d7ea64c0825954e6
guilleaer <guille.aer@gmail.com>
2016-09-28 13:48:13 +0200

    add browsersync commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer');
- 
+    autoprefixer = require('autoprefixer'),
+    browserSync = require('browser-sync').create();
+
 gulp.task('css', function () {
   var processors = [
     autoprefixer({browsers: ['last 10 chrome versions', 'last 10 Safari versions']})
@@ -10,3 +11,20 @@ gulp.task('css', function () {
     .pipe(postcss(processors))
     .pipe(gulp.dest('./dist/css'));
 });
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: "./dist"
+  });
+  gulp.watch("dist/css/*.css", ['css']);
+  gulp.watch("dist/*.html").on('change', browserSync.reload);
+});
+
+// Watch
+gulp.task('watch', function(){
+  gulp.watch('./src/css/*.css', ['css', browserSync.reload]);
+  gulp.watch('*.html', browserSync.reload);
+});
+
+gulp.task('default', ['css', 'browser-sync', 'watch']);
