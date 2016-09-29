31663b3d8c329c7171a406986c52825f0e417844
guilleaer <guille.aer@gmail.com>
2016-09-29 10:28:28 +0200

    Add new html task

--- gulpfile.js
+++ gulpfile.js
@@ -16,19 +16,22 @@ gulp.task('css', function () {
     .pipe(gulp.dest('./dist/css'));
 });
 
+gulp.task('html', function () {
+  return gulp.src('./src/*.html')
+    .pipe(gulp.dest('./dist'));
+});
+
 // Static server
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: "./dist"
   });
-  gulp.watch("dist/css/*.css", ['css']);
-  gulp.watch("dist/*.html").on('change', browserSync.reload);
 });
 
 // Watch
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   gulp.watch('./src/css/*.css', ['css', browserSync.reload]);
-  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('./src/*.html', ['html', browserSync.reload]);
 });
 
 gulp.task('default', ['css', 'browser-sync', 'watch']);
